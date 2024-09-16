780942954190d718462af1bcd5693cf0ce949659
avi-friedman-IL <155029199+avi-friedman-IL@users.noreply.github.com>
2024-09-16 16:59:40 +0300

    prod

--- config/index.js
+++ config/index.js
@@ -5,11 +5,11 @@ import configDev from './dev.js'
 export var config
 
 if (process.env.NODE_ENV === 'production') {
-  // config = configProd
-  config = configDev
+  config = configProd
+  // config = configDev
 } else {
-  // config = configProd
-  config = configDev
+  config = configProd
+  // config = configDev
 }
 config.isGuestMode = true
 
